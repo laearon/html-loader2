786cc43d0344d2d5f0717b95a8fbbcab4db6cab8
lcfme <lcfme@sina.com>
2018-07-13 14:48:22 +0800

    done

--- buildinPlugins.js
+++ buildinPlugins.js
@@ -1,4 +1,8 @@
 const { Transform } = require('stream');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+const base64 = require('base64-js');
 
 class HtmlBase64Plugin extends Transform {
     constructor(opts) {
@@ -11,10 +15,44 @@ class HtmlBase64Plugin extends Transform {
         callback();
     }
     _flush(callback) {
-        this._data = '<!-- HtmlLoader bundled -->\n' + this._data;
+        this._data = imgtag2base64(
+            this._data,
+            this._opts.filePath,
+            this._opts.basePath,
+            this._opts.size
+        );
         this.push(this._data);
         callback();
     }
 }
 
-module.exports = HtmlBase64Plugin;
+function imgtag2base64(html, filePath, basePath, size) {
+    var reg = /(<img[^<>]*?\s+src=["'])([^'"<>+]+?)(['"][^<>]*?>)/gi;
+    basePath = basePath || filePath;
+    return html.replace(reg, function($0, $1, $2, $3) {
+        try {
+            debugger;
+            var src = $2;
+            var dirname = path.dirname(filePath);
+            var destFile;
+            if (src[0] === '/') {
+                destFile = path.resolve(basePath, '.' + src);
+            } else {
+                destFile = path.resolve(dirname, src);
+            }
+            var extname = path.extname(destFile);
+            var fileData = fs.readFileSync(destFile);
+            if (typeof size === 'number' && fileData.byteLength > size * 1000)
+                throw new Error('file size exceeds max size allowed');
+            var base64Str = base64.fromByteArray(fileData);
+            var mimeInfo = mime.getType(extname);
+            if (!mimeInfo) throw new Error('Unknown mimeType');
+            base64Str = 'data:' + mimeInfo + ';base64,' + base64Str;
+            return $1 + base64Str + $3;
+        } catch (err) {
+            return $0;
+        }
+    });
+}
+
+exports.HtmlBase64Plugin = HtmlBase64Plugin;
--- test/HtmlLoder.test.js
+++ test/HtmlLoder.test.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const HTMLLoader = require('../');
+const HtmlBase64Plugin = require('../buildinPlugins').HtmlBase64Plugin;
 
 const loader = new HTMLLoader({
+    basePath: path.resolve(__dirname),
     filePath: path.resolve(__dirname, 'a.html'),
-    plugins: [[__dirname + '/fakePlugin', { a: 1 }]]
+    plugins: [[HtmlBase64Plugin, { size: 0 }]]
 });
 
 loader.on('finish', function(data) {
     debugger;
+    console.log(data);
 });
