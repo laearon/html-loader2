8a65ae61ef1cf815e0b61c5012c65c6be18a00df
lcfme <lcfme@sina.com>
2018-07-13 14:53:04 +0800

    done

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/bash
 
-node --inspect-brk bin.js --plugin [/Users/lcfme/Desktop/html-loader2/test/fakePlugin.js  --a=1] --filePath=/Users/lcfme/Desktop/html-loader2/test/a.html
-
-# node bin.js --plugin [/Users/lcfme/Desktop/html-loader2/test/fakePlugin.js  --a=1] --filePath=/Users/lcfme/Desktop/html-loader2/test/a.html
+node --inspect-brk bin.js --plugin [/Users/lcfme/Desktop/html-loader2/test/fakePlugin.js  --size=1] --filePath=/Users/lcfme/Desktop/html-loader2/test/a.html --basePath=/Users/lcfme/Desktop/html-loader2/test/
--- test/fakePlugin.js
+++ test/fakePlugin.js
@@ -1,6 +1,10 @@
 const { Transform } = require('stream');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+const base64 = require('base64-js');
 
-class Fake extends Transform {
+class HtmlBase64Plugin extends Transform {
     constructor(opts) {
         super();
         this._opts = opts;
@@ -11,11 +15,44 @@ class Fake extends Transform {
         callback();
     }
     _flush(callback) {
-        debugger;
-        this._data = '<!-- HtmlLoader bundled -->\n' + this._data;
+        this._data = imgtag2base64(
+            this._data,
+            this._opts.filePath,
+            this._opts.basePath,
+            this._opts.size
+        );
         this.push(this._data);
         callback();
     }
 }
 
-module.exports = Fake;
+function imgtag2base64(html, filePath, basePath, size) {
+    var reg = /(<img[^<>]*?\s+src=["'])([^'"<>+]+?)(['"][^<>]*?>)/gi;
+    basePath = basePath || filePath;
+    return html.replace(reg, function($0, $1, $2, $3) {
+        try {
+            debugger;
+            var src = $2;
+            var dirname = path.dirname(filePath);
+            var destFile;
+            if (src[0] === '/') {
+                destFile = path.resolve(basePath, '.' + src);
+            } else {
+                destFile = path.resolve(dirname, src);
+            }
+            var extname = path.extname(destFile);
+            var fileData = fs.readFileSync(destFile);
+            if (typeof size === 'number' && fileData.byteLength > size * 1000)
+                throw new Error('file size exceeds max size allowed');
+            var base64Str = base64.fromByteArray(fileData);
+            var mimeInfo = mime.getType(extname);
+            if (!mimeInfo) throw new Error('Unknown mimeType');
+            base64Str = 'data:' + mimeInfo + ';base64,' + base64Str;
+            return $1 + base64Str + $3;
+        } catch (err) {
+            return $0;
+        }
+    });
+}
+
+module.exports = HtmlBase64Plugin;
